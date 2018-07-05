15e60fe7afa61f599a9dbba9f8715e3bd828966a
M M <boss.ut4uya@gmail.com>
2018-07-05 20:09:52 +0300

    first

---
+++ dz/dz006/dz06.cs
@@ -0,0 +1,20 @@
+
+using System;
+
+namespace zad5
+{
+    class MainClass
+    {
+        public static void Main (string[] args){
+            int num;
+            Console.WriteLine ("Введите число");
+            num = convert.Toint32 Console.Read ();
+            for(int i=1;i<=num;i++){
+                int num_1=1;
+                num_1=num_1*i
+            }
+            Console.WriteLine ("{num}! = {num_1}")
+            
+        }
+    }
+}
